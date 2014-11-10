0724d0a764833c6c8c431dc86d7633a3d2a8d4ab
Carey Hiles <carey.hiles@gmail.com>
2014-11-10 15:23:35 +0000

    Self ref commit message from the file thats being commited

--- diln.sh
+++ diln.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 if [ -n "$1" ]
 then
-  status="$1"
-  gitcmd="git commit -m '$status' --allow-empty"
-  eval $gitcmd
-  `git push`
+    status="$1"
+    gitcmd="git commit -m '$status' --allow-empty"
+    eval $gitcmd
+    `git push`
 else
-  echo "Give us an update!!!"
+    echo "Give us an update!!!"
 fi
-
